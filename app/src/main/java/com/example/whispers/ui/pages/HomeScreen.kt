2cb1862c28331096b8b7164314cba2af2c50af6a
ellalearns <ellalearnscopy@gmail.com>
2024-10-27 19:06:04 +0100

    make the whispers column lazy column

--- app/src/main/java/com/example/whispers/ui/pages/HomeScreen.kt
+++ app/src/main/java/com/example/whispers/ui/pages/HomeScreen.kt
@@ -2,23 +2,38 @@ package com.example.whispers.ui.pages
 
 import android.graphics.Paint.Align
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.unit.dp
+import com.example.whispers.objects.dumbWhispers
+
+fun createDumbWhispers(): List<dumbWhispers> {
+    val dumbWhispers = mutableListOf<dumbWhispers>()
+    for (i in 1..20) {
+        dumbWhispers.add(dumbWhispers())
+    }
+    return dumbWhispers
+}
 
 @Composable
 fun HomeScreen() {
+
     Box(
         modifier = Modifier
             .background(Color.LightGray)
@@ -49,24 +64,42 @@ fun DateCol() {
 
 @Composable
 fun WhispersCol() {
-    Column {
-//        Text(text = "Whispers will be here")
-        WhisperCard()
-        WhisperCard()
-        WhisperCard()
+    val dumbWhispers = createDumbWhispers()
+
+    LazyColumn {
+        itemsIndexed (dumbWhispers) {index, item ->
+            WhisperCard(text = item.text, createdAt = item.createdBy, aiSummary = item.aiSummary)
+        }
     }
 }
 
 @Composable
-fun WhisperCard() {
+fun WhisperCard(
+    text: String,
+    createdAt: String,
+    aiSummary: String,
+) {
     Card (
         modifier = Modifier
             .fillMaxWidth()
             .height(80.dp)
             .padding(10.dp)
-//            .fillMaxHeight(0.1f)
     ) {
-        Text(text = "Whispers will be here")
+        Column (
+            modifier = Modifier
+                .padding(10.dp)
+        ) {
+            Text(text = text)
+
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = aiSummary)
+                Text(text = createdAt)
+            }
+        }
     }
 }
 
