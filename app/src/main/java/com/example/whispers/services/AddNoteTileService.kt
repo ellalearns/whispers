0c1c01ac390b835d18fdb769e65e21f539a1b0f5
ellalearns <ellalearnscopy@gmail.com>
2024-10-31 15:51:20 +0100

    add whisper quick settings alert dialog

--- app/src/main/java/com/example/whispers/services/AddNoteTileService.kt
+++ app/src/main/java/com/example/whispers/services/AddNoteTileService.kt
@@ -1,6 +1,22 @@
 package com.example.whispers.services
 
+import android.content.Intent
 import android.service.quicksettings.TileService
 
-public class AddNoteTileService: TileService() {
+class AddNoteTileService: TileService() {
+    override fun onTileAdded() {
+        qsTile.label = "Add Whisper"
+        qsTile.updateTile()
+    }
+
+    override fun onClick() {
+        showAddWhisperDialog()
+    }
+
+    private fun showAddWhisperDialog() {
+        val dialogIntent = Intent(this, WhisperDialogActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+        startActivity(dialogIntent)
+    }
 }
---
+++ app/src/main/java/com/example/whispers/services/WhisperDialogActivity.kt
@@ -0,0 +1,14 @@
+package com.example.whispers.services
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class WhisperDialogActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
