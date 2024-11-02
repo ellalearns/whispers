8da39fa9d304b23852494a4479fc44cabb52cdfb
ellalearns <ellalearnscopy@gmail.com>
2024-11-02 20:28:50 +0100

    finish qs launch app to add whisper

--- app/src/main/java/com/example/whispers/WhisperQSActivity.kt
+++ app/src/main/java/com/example/whispers/WhisperQSActivity.kt
@@ -1,11 +1,17 @@
 package com.example.whispers
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.example.whispers.objects.dumbWhispers
 import com.example.whispers.ui.pages.AddWhisperDialog
+import com.example.whispers.ui.pages.HomeScreen
 import com.example.whispers.ui.pages.whisperList
 
 class WhisperQSActivity : ComponentActivity() {
@@ -13,26 +19,16 @@ class WhisperQSActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            AddWhisperDialog(
-                onDismiss = {
-                    val intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                },
-                onAddNote = { newWhisper ->
-                    whisperList.add(
-                        dumbWhispers(
-                            text = newWhisper.text,
-                            createdBy = newWhisper.createdBy
-                        )
-                    )
-                    val intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                }
+
+            val showDialog = remember {
+                mutableStateOf(true)
+            }
+
+            HomeScreen(
+                showDialog = showDialog
             )
         }
+
     }
 }
-
 
